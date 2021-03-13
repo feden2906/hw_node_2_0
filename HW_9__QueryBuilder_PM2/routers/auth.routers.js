d267f9ee2f1b83d13e490b774be7448dbc1f4451
Fedenko Maksym <feden2906@gmail.com>
2021-03-13 21:54:42 +0200

    fixed =)

--- HW_9__QueryBuilder_PM2/routers/auth.routers.js
+++ HW_9__QueryBuilder_PM2/routers/auth.routers.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 const { authControllers } = require('../controllers');
-const { mwAuth, mwUrl } = require('../middlewares');
+const { mwAuth } = require('../middlewares');
 
 router.route('/')
-    .post(mwUrl.checkQuery, mwAuth.isUserExistForAuth, authControllers.authUser);
+    .post(mwAuth.isUserExistForAuth, authControllers.authUser);
 
 router.route('/refreshToken')
     .post(mwAuth.checkRefreshToken, authControllers.updateTokens);
