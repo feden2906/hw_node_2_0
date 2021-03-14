1f6d6b354c732aabf41c46f88d1367cf880cb57c
Fedenko Maksym <feden2906@gmail.com>
2021-03-14 10:56:07 +0200

    styled auth router

--- HW_9__QueryBuilder_PM2/routers/auth.routers.js
+++ HW_9__QueryBuilder_PM2/routers/auth.routers.js
@@ -4,9 +4,11 @@ const { authControllers } = require('../controllers');
 const { mwAuth } = require('../middlewares');
 
 router.route('/')
-    .post(mwAuth.isUserExistForAuth, authControllers.authUser);
+    .post(mwAuth.isUserExistForAuth,
+      authControllers.authUser);
 
 router.route('/refreshToken')
-    .post(mwAuth.checkRefreshToken, authControllers.updateTokens);
+    .post(mwAuth.checkRefreshToken,
+      authControllers.updateTokens);
 
 module.exports = router;
