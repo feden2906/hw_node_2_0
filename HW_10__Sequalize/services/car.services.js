bc74fe18122d39e5e658eb22507bf07a8a48eb77
Fedenko Maksym <feden2906@gmail.com>
2021-03-19 19:38:16 +0200

    car

--- HW_10__Sequalize/services/car.services.js
+++ HW_10__Sequalize/services/car.services.js
@@ -1,16 +1,16 @@
-const db = require('../dataBase').getInstance();
+// const db = require('../dataBase').getInstance();
 
-const { CAR } = require('../constants/modelNames.enum');
+// const { CAR } = require('../constants/modelNames.enum');
 
-module.exports = {
-  findCars: (query) => {
-    const Car = db.getModel(CAR);
-
-    return Car.findAll({ where: query });
-  }
+// module.exports = {
+  // findCars: (query) => {
+  //   const Car = db.getModel(CAR);
+  //
+  //   return Car.findAll({ where: query });
+  // }
 
 // CarById: (userID) => Car.findById(userID),
 // createCar: (carObject) => Car.create(carObject),
 // updateCar: (carID, carObject) => Car.findByIdAndUpdate(carID, { $set: carObject }),
 // deleteCar: (carID) => Car.findByIdAndDelete(carID)
-};
+// };
